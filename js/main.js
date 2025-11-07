a5a71495f030d3b3e2e5ce5bd27d4aed361deaf2
binarynumberz <111212400+binarynumberz@users.noreply.github.com>
2025-11-06 18:40:19 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,59 +10,62 @@ const config = {
   width: window.innerWidth,
   height: window.innerHeight,
   parent: 'game',
-  backgroundColor: '#e3d5b8',
+  backgroundColor: '#f2e3c6',
   scene: { preload, create, update }
 };
 
 const game = new Phaser.Game(config);
 
-function preload() {
-  // no external images needed
-}
+function preload() {}
 
 function create() {
-  // draw sand background
-  const bg = this.add.graphics();
-  bg.fillStyle(0xe3d5b8, 1);
-  bg.fillRect(0, 0, this.scale.width, this.scale.height);
+  // sand background
+  this.bg = this.add.graphics();
+  this.bg.fillStyle(0xe3d5b8, 1);
+  this.bg.fillRect(0, 0, this.scale.width, this.scale.height);
 
-  this.infoText = this.add.text(10, 10, 'Click to start your dig!', { fontSize: '20px', color: '#000', wordWrap: { width: this.scale.width - 20 } });
+  // info text at top
+  this.infoText = this.add.text(10, 10, 'Click the ground to dig!', { fontSize: '20px', color: '#000', wordWrap: { width: this.scale.width - 20 } });
 
+  // spawn the first artifact
   this.spawnArtifact();
 
-  this.input.on('pointerdown', pointer => this.checkDig(pointer));
+  // click handler
+  this.input.on('pointerdown', pointer => this.handleClick(pointer));
 }
 
 function spawnArtifact() {
-  const artifact = Phaser.Math.RND.pick(lessons);
-  this.currentArtifact = artifact;
+  this.currentArtifact = Phaser.Math.RND.pick(lessons);
 
-  const x = Phaser.Math.Between(100, this.scale.width - 100);
-  const y = Phaser.Math.Between(100, this.scale.height - 100);
+  // random location
+  this.artifactPos = {
+    x: Phaser.Math.Between(100, this.scale.width - 100),
+    y: Phaser.Math.Between(100, this.scale.height - 100)
+  };
 
-  const g = this.add.graphics();
-  g.fillStyle(Phaser.Display.Color.RandomRGB().color, 1);
-  g.fillCircle(x, y, 25);
-
-  this.artifactSprite = g;
-  this.artifactPos = { x, y };
+  // draw artifact as a circle
+  this.artifactSprite = this.add.graphics();
+  this.artifactSprite.fillStyle(0x8b0000, 1);
+  this.artifactSprite.fillCircle(this.artifactPos.x, this.artifactPos.y, 25);
 }
 
-function checkDig(pointer) {
+function handleClick(pointer) {
   const dist = Phaser.Math.Distance.Between(pointer.x, pointer.y, this.artifactPos.x, this.artifactPos.y);
 
+  // clicked on artifact
   if (dist < 30) {
-    this.infoText.setText(`🪓 You found a ${this.currentArtifact.name}!\n${this.currentArtifact.fact}\nClick to search again.`);
+    // show info box
+    alert(`🪓 You found a ${this.currentArtifact.name}!\n${this.currentArtifact.fact}`);
+
+    // destroy old artifact and spawn new one
     this.artifactSprite.destroy();
     this.spawnArtifact();
   } else {
-    this.infoText.setText('You dug here but found nothing. Try another spot.');
-
+    // small dig animation
     const hole = this.add.graphics();
     hole.fillStyle(0x9e8c68, 1);
     hole.fillCircle(pointer.x, pointer.y, 5);
     hole.alpha = 0.5;
-
     this.tweens.add({ targets: hole, alpha: 0, duration: 1200, onComplete: () => hole.destroy() });
   }
 }
